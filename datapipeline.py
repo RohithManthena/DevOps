4804941cd9a06932393090a2577343db72a03711
Surya Manthena <54606418+RohithManthena@users.noreply.github.com>
2020-09-21 16:27:52 -0500

    Update datapipeline.py

--- datapipeline.py
+++ datapipeline.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import subprocess
+from tabulate import tabulate
 
 # removing the name of this python file which is the first argument
 sys.argv.pop(0)
@@ -7,113 +9,78 @@ sys.argv.pop(0)
 #initializing a list that  has all the arguments 
 arguments=sys.argv
 
-#function to run all the files in a directory
-def execfiles_in_folder():
+#the first argument is the folder name which is mandatory
+folder=arguments[0]
+
+# if help is passed in the command line, all the commands will be displayed
+if "help" in arguments:
+    print("MANDATORY - Provide the folder name when executing the python script as ----> python datapipeline.py foldername")
+    print("OPTIONAL - If you want the files to executed in desc order use the keyword ----> python datapipeline.py foldername DESC")
+    print("OPTIONAL - If you want all the output to be printed in a file use the keyword o- followed by the file name----> python datapipeline.py foldername o-filename.txt")
+    print("The resut will be outputted in a file called output in the current directory")
+    print("OPTIONAL - If you want only one file to be executed in the directory give the file name preceeded by f- -----> python datapipeline.py foldername f-test.py")
     
-    #no arguments are given to the python script in run time
-    if len(arguments)==0:
-        sys.exit("Folder name is not given")
 
-    # only the folder is passed as argument
-    elif len(arguments)==1:
-        for root, dirs, files in os.walk("D:\\", topdown=False):
-            for i in dirs:
-                if i==folder:
-                    path=root+"\\"+folder
-                    break
-        files = os.listdir(path)
-        for file_ in files:
-            exec(open(path+"\\"+file_).read())
-
-
-
-    elif len(arguments)==2:
-        if arguments[1] != "desc":
-            sys.exit("script order ")
-    elif len(arguments)==3:
-    elif len(arguments)==4:
-    elif len(arguments)==5:
-    else:
-        sys.exit("max 5 arguments should be passed")
-
+else:
+    # finding the folder path using os.walk
     for root, dirs, files in os.walk("D:\\", topdown=False):
         for i in dirs:
             if i==folder:
                 path=root+"\\"+folder
                 break
-    files = os.listdir(path)
-    for file_ in files:
-        exec(open(path+"\\"+file_).read())  
+    consoleoutput=list(tuple())
+    headers = ["folder","file"]
+    #checking if any file command is given
+    fileinput='f-'
+    fileinput_provided=[i for i in arguments if fileinput in i] 
+
+    #checking if any output command is given
+    outputfile='o-'
+    outputfile_provided=[i for i in arguments if outputfile in i]
+
+    # if the file command is passed
+    if len(fileinput_provided)==1:
+        #if the output command is passed
+        if len(outputfile_provided)==1:
+            #open a read file and put the output there
+            with open(outputfile_provided[0][2:], "wb") as f:
+                subprocess.check_call(["python", path+"\\"+fileinput_provided[0][2:]], stdout=f)
+                consoleoutput.append((folder,fileinput_provided[0][2:]))
+        else:
+            exec(open(path+"\\"+arguments).read())
+            consoleoutput.append((folder,fileinput_provided[0][2:]))
+
+    
+    else:
+        #open a read file and put the output there
+        if len(outputfile_provided)==1:
+            with open(outputfile_provided[0][2:], "wb") as f:
+            #run files in descending order
+                if "DESC" in arguments:
+                    files=os.listdir(path)
+                    files.reverse()
+                    for file_ in files:
+                        print(type(file_))
+                        subprocess.check_call(["python", path+"\\"+file_], stdout=f)
+                        consoleoutput.append((folder,file_))
+                else:
+                    files=os.listdir(path)
+                    for file_ in files:
+                        subprocess.check_call(["python", path+"\\"+file_], stdout=f)
+                        consoleoutput.append((folder,file_))
+
+        else:
+            if "desc" in arguments:
+                files=os.listdir(path)
+                files.reverse()
+                for file_ in files:
+                    exec(open(path+"\\"+file_).read())
+                    consoleoutput.append((folder,file_))
+            else:
+                files=os.listdir(path)
+                for file_ in files:
+                    exec(open(path+"\\"+file_).read())
+                    consoleoutput.append((folder,file_))
 
 
-
-# removing the name of this python file which is the first argument
-sys.argv.pop(0)
-
-#initializing a list that  has all the arguments 
-arguments=sys.argv
-
-if len(arguments)==0:
-    sys.exit("Folder name is not given")
-
-# only the folder is passed as argument
-elif len(arguments)==1:
-    execfiles_in_folder(arguments[0])
-
-
-
-elif len(arguments)==2:
-elif len(arguments)==3:
-elif len(arguments)==4:
-elif len(arguments)==5:
-else:
-    sys.exit("max 5 arguments should be passed")
-
-    pass
-#
-
-while True:
-    folder=input("Input the folder name:")
-    if folder:
-        break
-script_order=input("Do you want the files to be executed in descending ? answer with 'yes' or 'no'")
-#while script_order!="yes" or script_order!="no":
-
-
-for root, dirs, files in os.walk("D:\\", topdown=False):
-    for i in dirs:
-        if i==folder:
-            path=root+"\\"+folder
-            break
-files = os.listdir(path)
-for file_ in files:
-    exec(open(path+"\\"+file_).read())
-
-
-
-
-
-# def find_files(search_path):
-#    result = []
-
-# # Wlaking top-down from the root
-#    for root, dir, files in os.walk(search_path):
-#       if filename in files:
-#          result.append(os.path.join(root, filename))
-#    return result
-
-# print(find_files("smpl.htm","D:"))
-
-
-#    for name in files:
-#       print(os.path.join(root, name))
-#    for name in dirs:
-#       print(os.path.join(root, name))
-
-# path = 'D:\STUDY\COURSEWORK\DATA SCIENCE PROGRAMMING\ASSIGNMENTS\PRSA_Data_20130301-20170228'
-# for file in os.listdir(path):
-#     if (file != 'PRSA_Data_Aotizhongxin_20130301-20170228.csv'):
-#         filepath="PRSA_Data_20130301-20170228/"+file
-#         data=pd.read_csv(filepath)
-#         df=df.append(data)
-
+print(tabulate(consoleoutput,headers=headers))
